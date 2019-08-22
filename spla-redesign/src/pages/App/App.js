97fa8b88355b6b89ce1a3c57b84f33a2c9f8286f
Casey Brannon <cbrannon412@gmail.com>
2019-08-21 21:17:56 -0700

    Pages

--- spla-redesign/src/pages/App/App.js
+++ spla-redesign/src/pages/App/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import "./App.css";
 import { Route, Switch, Redirect } from "react-router-dom";
+import Button from "../../components/Button/Button";
 import Footer from "../../components/Footer/Footer";
 import Header from "../../components/Header/Header";
 
@@ -16,6 +17,7 @@ class App extends Component {
         <div className="site-content">
         <Header />
         <h1>Hello World</h1>
+        <Button />
       </div>
       <Footer />
       </div>
---
+++ spla-redesign/src/pages/OptionsPage/OptionsPage.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './OptionsPage.css'
+
+const OptionsPage = props => {
+    return (
+        <div>
+            <h1>Hello from options</h1>
+        </div>
+    );
+}
+
+export default OptionsPage;
