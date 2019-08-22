87a2f6107a65401046e985769bae1f8508624f07
Jessica Choi <jessica@Jessicas-MacBook-Air.local>
2019-08-22 12:33:44 -0700

    home page and button changes

--- spla-redesign/src/components/Button/Button.jsx
+++ spla-redesign/src/components/Button/Button.jsx
@@ -1,13 +1,16 @@
-import React from 'react';
-import './Button.css';
-import { Link } from 'react-router-dom';
+import React from "react";
+import "./Button.css";
+import { Link } from "react-router-dom";
+import { Row, Col } from "react-bootstrap";
 
 const Button = props => {
-    return (
-        <div>
-            <button>I'm a button!</button>
-        </div>
-    )
-}
+  return (
+    <Row>
+      <Col xs={{ span: 6, offset: 3 }} lg={{ span: 4, offset: 2 }}>
+        <button className="btn">I'm a button!</button>
+      </Col>
+    </Row>
+  );
+};
 
 export default Button;
--- spla-redesign/src/components/Footer/Footer.jsx
+++ spla-redesign/src/components/Footer/Footer.jsx
@@ -6,31 +6,35 @@ import { Grid, Row, Col } from "react-bootstrap";
 const Footer = props => {
   return (
     <div className="footer">
-      <Row className="justify-content-md-center">
-        <Col xs={12} md={4} className="sm-links">
+      <Row className="justify-content-xs-center">
+        <Col
+          xs={{ span: 6, offset: 3 }}
+          md={{ span: 3, order: 12, offset: 3 }}
+          className="sm-links"
+        >
           <a
             href="https://www.facebook.com/SafeParkingLA/"
             target="_blank"
             className="sm-link"
           >
-            <i class="fab fa-facebook" />
+            <i className="fab fa-facebook" />
           </a>
           <a
             href="http://instagram.com/safeparking_la"
             target="_blank"
             className="sm-link"
           >
-            <i class="fab fa-instagram" />
+            <i className="fab fa-instagram" />
           </a>
           <a
             href="mailto:info@safeparkingla.org"
             target="_blank"
             className="sm-link"
           >
-            <i class="far fa-envelope" />
+            <i className="far fa-envelope" />
           </a>
         </Col>
-        <Col xs={12} md={4} className="footer-links">
+        <Col xs={12} md={{ span: 6, order: 1 }} className="footer-links">
           <Link to="/About" className="footer-link">
             About
           </Link>
