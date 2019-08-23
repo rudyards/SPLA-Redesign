28aee7dd1f9e6825b70fcfc2c4377feada588656
Casey Brannon <cbrannon412@gmail.com>
2019-08-22 23:50:28 -0700

    header on pages

--- spla-redesign/src/pages/AboutPage/AboutPage.jsx
+++ spla-redesign/src/pages/AboutPage/AboutPage.jsx
@@ -1,9 +1,11 @@
 import React from "react";
 import "./AboutPage.css";
+import Header from '../../components/Header/Header'
 
 const AboutPage = props => {
   return (
     <div>
+    <Header />
       <h1>Hello from aboutPage</h1>
     </div>
   );
