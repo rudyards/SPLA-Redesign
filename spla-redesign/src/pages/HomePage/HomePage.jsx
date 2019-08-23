c16a267d7d0e7d7a664274d144c20501bfbd55ad
Jessica Choi <jessica@Jessicas-MacBook-Air.local>
2019-08-22 17:07:49 -0700

    buttons and homepage

--- spla-redesign/src/pages/HomePage/HomePage.jsx
+++ spla-redesign/src/pages/HomePage/HomePage.jsx
@@ -4,6 +4,7 @@ import "./HomePage.css";
 import Header from "../../components/Header/Header";
 import { Row, Col } from "react-bootstrap";
 import Button from "../../components/Button/Button";
+import { Link } from "react-router-dom";
 
 const HomePage = props => {
   return (
@@ -19,8 +20,10 @@ const HomePage = props => {
                 You’ve taken the first step towards help. <br />
                 You’re on the right track
               </p>
+              <Link to="/options" className="btn">
+                Get Started
+              </Link>
             </Col>
-            <Button />
           </Col>
         </Row>
       </div>
@@ -36,7 +39,21 @@ const HomePage = props => {
             interest form provided or call/text <br />{" "}
             <span className="phone-num">213-793-8493</span>
           </p>
-          <Button />
+          <Button buttonName={"About"} />
+        </Col>
+      </Row>
+      <Row>
+        <img
+          src="https://i.imgur.com/TPc9Dc0.jpg"
+          alt="Safe Parking LA"
+          className="home-story-img"
+        />
+        <Col xs={12} className="home-story">
+          <p className="story">
+            In August 2018, after two years of living in their car, Lawrence and
+            Carla McCue finally picked up the keys to their very own apartment.
+          </p>
+          <Button buttonName={"Read More"} />
         </Col>
       </Row>
     </div>
