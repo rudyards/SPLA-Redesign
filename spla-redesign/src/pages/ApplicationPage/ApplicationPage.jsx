eaed3d95d8f4da6a6dcd02dd7d6fc928cbde6041
Jessica Choi <jessica@Jessicas-MacBook-Air.local>
2019-08-22 23:10:00 -0700

    add application page

--- spla-redesign/src/pages/ApplicationPage/ApplicationPage.jsx
+++ spla-redesign/src/pages/ApplicationPage/ApplicationPage.jsx
@@ -1,9 +1,23 @@
 import React from "react";
+import Header from "../../components/Header/Header";
+import { NONAME } from "dns";
 
 const ApplicationPage = props => {
   return (
     <div>
-      <h1>hi</h1>
+      <Header />
+      <iframe
+        className="airtable-embed"
+        src="https://airtable.com/embed/shrpKxqeKVGHWr1AT?backgroundColor=orange"
+        style={{
+          width: "100%",
+          height: "500px",
+          background: "transparent",
+          border: "none",
+          frameBorder: "0",
+          onMouseWheel: ""
+        }}
+      />
     </div>
   );
 };
